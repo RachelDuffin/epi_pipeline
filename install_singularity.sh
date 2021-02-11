ef2ebeb9d1ee05cb801400ac910c707ec433f8a2
Rachel Duffin <rachel.g.duffin@gmail.com>
2021-02-11 10:51:51 +0000

    Add checkinstall to make singualarity removal easier

--- install_singularity.sh
+++ install_singularity.sh
@@ -5,19 +5,20 @@ v="$(singularity --version)"
 if $v='singularity version 3.5.3'; then
   echo "singularity v3.5.3 already installed"
 else
+  # Install checkinstall
+  sudo apt-get update && sudo apt-get install checkinstall
   # Install dependencies
-  sudo apt-get update && sudo apt-get install -y \ build-essential \ libssl-dev \ uuid-dev \ libgpgme11-dev \
-    squashfs-tools \ libseccomp-dev \ pkg-config \ golang
+  sudo apt-get update && sudo apt-get install -y build-essential libssl-dev uuid-dev libgpgme11-dev squashfs-tools libseccomp-dev pkg-config golang
   # Download and compile singularity 3.5.3
-  sudo mkdir -p /usr/local/go/src/github.com/sylabs && \
+  mkdir -p /usr/local/go/src/github.com/sylabs && \
   cd /usr/local/go/src/github.com/sylabs && \
-  sudo wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
-  sudo tar -xzf singularity-${VERSION}.tar.gz && \
-  cd ./singularity && \
-  sudo ./mconfig && \
+  wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
+  tar -xzf singularity-${VERSION}.tar.gz && \
+  cd singularity && \
+  ./mconfig && \
   cd builddir && \
-  sudo make && \
-  sudo make install && \
+  make && \
+  sudo checkinstall && \
   cd /usr/local/go/src/github.com/sylabs && \
-  sudo rm singularity-${VERSION}.tar.gz
+  rm singularity-${VERSION}.tar.gz
 fi
