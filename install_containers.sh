a5927641660546cdb566abece57ca8e6fa8b982d
Rachel Duffin <rachel.g.duffin@gmail.com>
2021-02-13 16:26:55 +0000

    Update install_containers.sh

--- install_containers.sh
+++ install_containers.sh
@@ -6,21 +6,24 @@ cd apps
 
 echo "INSTALLING QC TOOLS"
 echo "Installing FastQC"
-singularity pull --name fastqc docker://quay.io/biocontainers/fastqc:0.11.9--0
+singularity pull fastqc.sif docker://quay.io/biocontainers/fastqc:0.11.9--0
 echo "Installing MultiQC"
-singularity pull --name multiqc docker://quay.io/biocontainers/multiqc:1.9--py_1
+singularity pull multiqc.sif docker://quay.io/biocontainers/multiqc:1.9--py_1
 echo "Installing PycoQC"
-singularity pull --name pycoqc docker://quay.io/biocontainers/pycoqc:2.5.2--py_0
+singularity pull pycoqc.sif docker://quay.io/biocontainers/pycoqc:2.5.2--py_0
 
 
 echo "INSTALLING PROCESSING TOOLS"
 echo "Installing Minimap2"
-singularity pull --name minimap2 docker://quay.io/biocontainers/minimap2:2.17--hed695b0_3
+singularity pull minimap2.sif docker://quay.io/biocontainers/minimap2:2.17--hed695b0_3
 echo "Installing Samtools"
-singularity pull --name samtools docker://quay.io/biocontainers/samtools:0.1.18--hfb9b9cc_10
+singularity pull samtools.sif docker://quay.io/biocontainers/samtools:0.1.18--hfb9b9cc_10
 echo "Installing bam2fastx"
-singularity pull --name bam2fastx docker://quay.io/biocontainers/bam2fastx:1.3.1--he1c1bb9_0
+singularity pull bam2fastx.sif docker://quay.io/biocontainers/bam2fastx:1.3.1--he1c1bb9_0
 echo "Installing Medaka"
-singularity pull --name medaka docker://quay.io/biocontainers/medaka:1.2.1--py38hfcf0ad1_0
+singularity pull medaka.sif docker://quay.io/biocontainers/medaka:1.2.1--py38hfcf0ad1_0
 
 cd ..
+
+
+
