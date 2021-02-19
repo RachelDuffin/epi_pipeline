d4ac21883e4f2d5a17cada8d4381cfe0e7258df7
Rachel Duffin <k1927570@login3.pri.rosalind2.alces.network>
2021-02-19 09:14:45 +0000

    Change multiple elements to ensure pipeline runs on HPC

--- pipeline.py
+++ pipeline.py
@@ -1,45 +1,52 @@
 import glob
+import sys
 import os
 import subprocess
+import re
 import install_containers
 
-
-def get_identifier(runfolder, file, string):
+def get_identifier(file, string):
     """
     Gets the run id from the run's final_summary file
     """
-    with open("data/run_folders/" + runfolder + "/" + file, "rt") as file_contents:
+    with open(file, "rt") as file_contents:
         lines = file_contents.readlines()
         for line in lines:
             if line.__contains__(string):
-                identifier = (line.split("=", 1))[0]
+                id = (line.split("=", 1))[1]
+    if ".fq" in file:
+        id = id.split()[-1].split("=")[-1]
+    identifier = id
     return identifier
 
 
-def create_directory(parent_directory, directory_list):
+def create_directory(parent_directory, directory):
     """
     Create output directories
     """
-    for i in directory_list:
-        if os.path.exists(parent_directory + i):
-            print(i + "directory already exists")
-        else:
-            os.mkdir(os.path.join(parent_directory, i))
-            print("output folder " + i + "created")
+    if os.path.exists(parent_directory + "/" + directory):
+        print(directory + " directory already exists")
+    else:
+        os.mkdir(os.path.join(parent_directory, directory))
+        print("output folder " + directory + "created")
 
 
 def fastqc(run_folder, run_id):
     """
     FastQC analysis per run folder
     """
+    print("--------------------------\nFASTQC\n--------------------------")
     output_filename = run_id + "_24hrs_fastqc.html"
     # if output file already present, do not re-analyse
     if os.path.isfile("output/" + run_folder + "/fastqc/" + output_filename):
         print("FastQC output for run " + run_id + " already exists")
     else:
         print("Creating fastqc file for run " + run_id)
-        subprocess.run("singularity exec apps/fastqc.sif fastqc data/run_folders/" + run_folder +
-                       "_24hrs.fq -o output/fastqc", shell=True)
+        for file in glob.glob("data/run_folders/" + run_folder + "*_24hrs.fq"):
+            barcode = get_identifier(file=file, string="barcode")
+            subprocess.run("module load apps/singularity; singularity exec apps/fastqc.sif fastqc data/run_folders/" + run_folder +
+                       barcode + "_24hrs.fq -o output/" + run_folder + "/fastqc", shell=True)
+    print("--------------------------")
 
 
 def pycoqc(run_folder):
@@ -47,28 +54,29 @@ def pycoqc(run_folder):
     Using guppy barcoding file. Checks if barcodes already split, if not calls split_barcodes() function.
     Then creates a pycoQC json report per barcode.
     """
-    # If barcodes not yet split, split the summary sequencing files according to barcodes
+    print("--------------------------\nPYCOQC\n--------------------------")
+    # If pycoQC not yet run, split the summary sequencing files according to barcodes and run pycoQC
     if not os.listdir("output/" + run_folder + "/pycoqc"):
-        print("Summary sequencing files not yet split")
+        print("pycoQC not yet run")
         split_barcodes(run_folder)
+        for file in glob.glob("output/" + run_folder + "/pycoqc/sequencing_summary_*"):
+            # get barcode name from barcode_split output file names
+            barcode = file.split(".", 1)[0].split("summary_", 1)[1]
+            print("Creating pycoQC json report for " + file)
+            subprocess.run(
+                "module load apps/singularity; singularity exec apps/pycoqc.sif pycoQC -f " + file +
+                " --json_outfile output/" + run_folder + "/pycoqc/" + barcode + "_pycoQC_output.json", shell=True)
     else:
-        print("Directory not empty - barcodes already split")
-
-    for file in glob.glob("output/" + run_folder + "/pycoqc/sequencing_summary_*"):
-        # get barcode name from barcode_split output file names
-        barcode = subprocess.run("echo " + file + " | cut -d '_' -f 3 | cut -d '.' -f 1")
-        print("Creating pycoQC json report for " + file)
-        subprocess.run(
-            "singularity exec apps/pycoqc.sif pycoQC -f " + file +
-            " --json_outfile output/" + run_folder + "/pycoqc/" + barcode + "_pycoQC_output.json", shell=True)
+        print("Directory not empty - pycoQC already run")
+    print("--------------------------")
 
 
 def split_barcodes(run_folder):
     for file in glob.glob("data/run_folders/" + run_folder + "/sequencing_summary_*"):
         print("Splitting summary sequencing file " + file + " according to barcodes")
         subprocess.run(
-            "singularity exec apps/pycoqc.sif Barcode_split --output_unclassified --min_barcode_percent 0.0 " +
-            "--summary_file " + "data/run_folders/" + run_folder + "/" + file + " --output_dir output/pycoqc/",
+            "module load apps/singularity; singularity exec apps/pycoqc.sif Barcode_split --output_unclassified " +
+            "--min_barcode_percent 0.0 --summary_file " + file + " --output_dir output/" + run_folder + "/pycoqc",
             shell=True)
 
 
@@ -77,13 +85,16 @@ def human_read_removal(ref, run_folder, run_id):
     Removes human reads from the samples by alignment to the human reference genome.
 
     """
+    print("--------------------------\nHUMAN READ REMOVAL\n--------------------------")
     for file in glob.glob("data/run_folders/" + run_folder + "/*.fq"):
-        barcode = get_identifier(runfolder=run_folder, file=file, string="barcode")
+        barcode = get_identifier(file=file, string="barcode")
         # align reads to human reference genome
         print("Aligning reads to human reference genome for " + file)
         subprocess.run(
-            "singularity exec apps/minimap2.sif minimap2 -ax map-ont " + ref + file +
-            " > output/human_read_removal/" + run_id + "_" + barcode + "_aligned.sam", shell=True)
+            "module load apps/singularity; singularity exec apps/minimap2.sif minimap2 -ax map-ont " + ref + " " +
+            file + " > output/" + run_folder + "/human_read_removal/" + run_id + "_" + barcode + "_aligned.sam",
+            shell=True)
+    print("--------------------------")
         # export unassigned reads to bam file with samtools
         # singularity shell apps/samtools -c "samtools view -f 4 file.bam > unmapped.sam"
 
@@ -97,29 +108,30 @@ def multiqc():
     """
     Create MultiQC report, pulling in outputs from other tools
     """
+    print("--------------------------\nMULTIQC\n--------------------------")
     print("Creating MultiQC report for the analysis")
-    subprocess.run("singularity exec apps/multiqc.sif python -m multiqc output --outdir output/multiqc", shell=True)
-
+    subprocess.run("module load apps/singularity; singularity exec apps/multiqc.sif python -m multiqc output --outdir output/multiqc", shell=True)
+    print("--------------------------")
 
 def main():
     # Install containers
     install_containers.install_tools()
     # Load singularity
-    subprocess.run("module load apps/singularity", shell=True)
-    for directory in os.listdir("data/run_folders"):
-        if os.path.isdir("data/run_folders/" + directory):
-            summary_file=glob.glob("data/run_folders/" + directory + "/final_summary_*.txt")
-            run_id = get_identifier(runfolder=directory, file=summary_file, string="sample_id=")
-            # create output directory per run, and subdirectories for outputs from each tool
-            create_directory(parent_directory="output", directory_list=run_id)
-            sub_directories = ["fastqc", "pycoqc", "human_read_removal"]
-            create_directory(parent_directory="output/" + run_id, directory_list=sub_directories)
-            # Conduct fastQC analysis
-            fastqc(run_folder=directory, run_id=run_id)
-            # Conduct pycoQC analysis
-            pycoqc(run_folder=directory)
-            reference_genome = "data/human_genome/ncbi/GCF_000001405.39_GRCh38.p13_genomic.fna"
-            human_read_removal(ref=reference_genome, run_folder=dir, run_id=run_id)
+    for entry in os.listdir("data/run_folders"):
+        if os.path.isdir("data/run_folders/" + entry):
+            for summary_file in glob.glob("data/run_folders/" + entry + "/final_summary_*.txt"):
+                run_id = get_identifier(file=summary_file, string="sample_id=").rstrip("\n")
+                # create output directory per run, and subdirectories for outputs from each tool
+                create_directory(parent_directory="output", directory=run_id)
+                sub_directories = ["fastqc", "pycoqc", "human_read_removal"]
+                for i in sub_directories:
+                    create_directory(parent_directory="output/" + run_id, directory=i)
+                # Conduct fastQC analysis
+                fastqc(run_folder=entry, run_id=run_id)
+                # Conduct pycoQC analysis
+                pycoqc(run_folder=entry)
+                reference_genome = "data/human_genome/ncbi/GCF_000001405.39_GRCh38.p13_genomic.fna"
+                human_read_removal(ref=reference_genome, run_folder=entry, run_id=run_id)
 
 
 if __name__ == '__main__':
