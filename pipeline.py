37dae20476250d39defde7bae575b2fc70659834
Rachel Duffin <k1927570@nodeb18.pri.rosalind2.alces.network>
2021-02-24 14:20:32 +0000

    Fix fastqc and alignment stages - now fully working

--- pipeline.py
+++ pipeline.py
@@ -1,10 +1,13 @@
 import glob
-import sys
 import os
-import subprocess
 import re
+import subprocess
+import sys
+
 import install_containers
 
+
+# SETTINGS
 def get_identifier(file, string):
     """
     Gets the run id from the run's final_summary file
@@ -28,126 +31,161 @@ def create_directory(parent_directory, directory):
         print(directory + " directory already exists")
     else:
         os.mkdir(os.path.join(parent_directory, directory))
-        print("output folder " + directory + "created")
+        print("output folder " + directory + " created")
 
 
-def fastqc(run_folder, run_id):
+def fastqc(data_dir, out_dir, run_id):
     """
     FastQC analysis per run folder
     """
     print("--------------------------\nFASTQC\n--------------------------")
     output_filename = run_id + "_24hrs_fastqc.html"
     # if output file already present, do not re-analyse
-    if os.path.isfile("output/" + run_folder + "/fastqc/" + output_filename):
+    if os.path.isfile(out_dir + "/fastqc/" + output_filename):
         print("FastQC output for run " + run_id + " already exists")
     else:
         print("Creating fastqc file for run " + run_id)
-        for file in glob.glob("data/run_folders/" + run_folder + "*_24hrs.fq"):
+        for file in glob.glob(data_dir + "/*_24hrs.fq"):
             barcode = get_identifier(file=file, string="barcode")
-            subprocess.run("module load apps/singularity; singularity exec apps/fastqc.sif fastqc data/run_folders/" + run_folder +
-                       barcode + "_24hrs.fq -o output/" + run_folder + "/fastqc", shell=True)
+            subprocess.run("module load apps/singularity; singularity exec apps/fastqc.sif fastqc " + file + " -o " +
+                           out_dir + "/fastqc", shell=True)
     print("--------------------------")
 
 
-def pycoqc(run_folder):
+def pycoqc(data_dir, out_dir):
     """
     Using guppy barcoding file. Checks if barcodes already split, if not calls split_barcodes() function.
     Then creates a pycoQC json report per barcode.
     """
     print("--------------------------\nPYCOQC\n--------------------------")
     # If pycoQC not yet run, split the summary sequencing files according to barcodes and run pycoQC
-    if not os.listdir("output/" + run_folder + "/pycoqc"):
+    if not os.listdir(out_dir + "/pycoqc"):
         print("pycoQC not yet run")
-        split_barcodes(run_folder)
-        for file in glob.glob("output/" + run_folder + "/pycoqc/sequencing_summary_*"):
+        split_barcodes(data_dir=data_dir, out_dir=out_dir)
+        for file in glob.glob(out_dir + "/pycoqc/sequencing_summary_*"):
             # get barcode name from barcode_split output file names
             barcode = file.split(".", 1)[0].split("summary_", 1)[1]
             print("Creating pycoQC json report for " + file)
             subprocess.run(
                 "module load apps/singularity; singularity exec apps/pycoqc.sif pycoQC -f " + file +
-                " --json_outfile output/" + run_folder + "/pycoqc/" + barcode + "_pycoQC_output.json", shell=True)
+                " --json_outfile " + out_dir + "/pycoqc/" + barcode + "_pycoQC_output.json", shell=True)
     else:
         print("Directory not empty - pycoQC already run")
     print("--------------------------")
 
 
-def split_barcodes(run_folder):
-    for file in glob.glob("data/run_folders/" + run_folder + "/sequencing_summary_*"):
+def split_barcodes(data_dir, out_dir):
+    for file in glob.glob(data_dir + "/sequencing_summary_*"):
         print("Splitting summary sequencing file " + file + " according to barcodes")
         subprocess.run(
             "module load apps/singularity; singularity exec apps/pycoqc.sif Barcode_split --output_unclassified " +
-            "--min_barcode_percent 0.0 --summary_file " + file + " --output_dir output/" + run_folder + "/pycoqc",
+            "--min_barcode_percent 0.0 --summary_file " + file + " --output_dir " + out_dir + "/pycoqc",
             shell=True)
 
 
-def human_read_removal(ref, run_folder, run_id):
+def human_read_removal(data_dir, out_dir, run_id, ref):
     """
     Removes human reads from the samples by alignment to the human reference genome.
-
     """
     print("--------------------------\nHUMAN READ REMOVAL\n--------------------------")
-    for file in glob.glob("data/run_folders/" + run_folder + "/*.fq"):
+    for file in glob.glob(data_dir + "/*.fq"):
         barcode = get_identifier(file=file, string="barcode")
-        if os.path.isfile("output/" + run_folder + "/human_read_removal/" + run_folder + "_" + barcode + "_aligned.sam"):
+        out_path = out_dir + "/human_read_removal/" + run_id + "_" + barcode
+        if os.path.isfile(out_path + "_aligned.sam"):
             print(barcode + " already aligned")
         else:
             # align reads to human reference genome using ont-specific parameters
             print("Aligning reads to human reference genome for " + file)
             subprocess.run(
                 "module load apps/singularity; singularity exec apps/minimap2.sif minimap2 -ax map-ont " + ref + " " +
-                file + " > output/" + run_folder + "/human_read_removal/" + run_id + "_" + barcode + "_aligned.sam",
-                shell=True)
-        # import SAM to BAM as @SQ lines present in header, only import unassigned reads (non-human)
-        print("Import non-human reads as BAM file for output/" + run_folder + "/human_read_removal/" + run_id + "_"
-              + barcode + "_aligned.sam")
-        subprocess.run(
-            "module load apps/singularity; singularity exec apps/samtools.sif samtools view -bS -f 4 " + "output/" +
-            run_folder + "/human_read_removal/" + run_id + "_" + barcode + "_aligned.sam" + " > " + "output/" +
-            run_folder + "/human_read_removal/" + run_id + "_" + barcode + "_unmapped.bam", shell=True)
-        # convert bam to fastq file
-        print("Convert bam to fastq file for Import non-human reads as BAM file for output/" +
-            run_folder + "/human_read_removal/" + run_id + "_" + barcode + "_unmapped.bam")
-        subprocess.run(
-            "module load apps/singularity; singularity exec apps/samtools.sif samtools bam2fq output/" +
-            run_folder + "/human_read_removal/" + run_id + "_" + barcode + "_unmapped.bam > output/" +
-            run_folder + "/human_read_removal/" + run_id + "_" + barcode + "_unmapped.fastq", shell=True)
+                file + " > " + out_path + "_aligned.sam", shell=True)
+        if os.path.isfile(out_path + "_unmapped.fastq"):
+            print("Human read removal already complete for " + barcode)
+        else:
+            # import SAM to BAM as @SQ lines present in header, only import unassigned reads (non-human)
+            print("Import non-human reads as BAM file for " + out_path + "_aligned.sam")
+            subprocess.run(
+                "module load apps/singularity; singularity exec apps/samtools.sif samtools view -bS -f 4 " + out_path +
+                "_aligned.sam" + " > " + out_path + "_unmapped.bam", shell=True)
+            # convert bam to fastq file
+            print("Convert bam to fastq file for Import non-human reads as BAM file for " + out_path + "_unmapped.bam")
+            subprocess.run(
+                "module load apps/singularity; singularity exec apps/samtools.sif samtools bam2fq " + out_path +
+                "_unmapped.bam > " + out_path + "_unmapped.fastq", shell=True)
+            # remove intermediary file
+            os.remove(out_path + "_unmapped.bam")
     print("--------------------------")
 
-        # minimap2 for mapping alignment, bcftools consensus generation, SNP-sites to identify SNPs between samples
-        # multi-locus sequence typing using MLST, and SNP-dists to calculate SNP distances.1
 
-def de_novo_assembly():
-    pass
+def de_novo_assembly(data_dir, out_dir, run_id):
+    # --meta enables mode for metagenome/uneven coverage assembly, designed for highly non-uniform coverage and
+    # is sensitive to underrepresented sequence at low coverage (as low as 2x)
+    # alternative haplotypes are collapsed
+    # minimum overlap length is chosen automatically based on read length distribution
+    # flye performs one polishing iteration by default
+    print("--------------------------\nDE NOVO ASSEMBLY\n--------------------------")
+    for data_input in glob.glob(data_dir + "/*.fq"):
+        barcode = get_identifier(file=data_input, string="barcode")
+        for file in glob.glob(out_dir + "/human_read_removal/" + run_id + "*" + barcode + "*.fastq"):
+            if os.path.exists(out_dir + "/de_novo_assembly/" + run_id + "_" + barcode):
+                print("De novo assembly already complete for " + barcode)
+            else:
+                print("Performing de novo assembly for " + file)
+                create_directory(parent_directory=out_dir + "/de_novo_assembly", directory=run_id + "_" + barcode)
+                print("Conducting de novo assembly for " + file)
+                subprocess.run("module load apps/singularity; singularity exec apps/flye.sif flye --nano-raw " +
+                                file + " --out-dir " + out_dir + "/de_novo_assembly/" + run_id + "_" + barcode +
+                                " --meta", shell=True)
+    print("--------------------------")
+
 
 def mapping_assembly():
+    # start with minimap2
     pass
+
 
 def consensus_generation():
+    # bcftools consensus generation
     pass
+
 
 def variant_calling():
+    # SNP-sites to identify SNPs between samples
     pass
+
 
 def genetic_distance():
+    # SNP-dists to calculate SNP distances
     pass
 
+
 def mlst():
+    # multi-locus sequence typing using MLST
     pass
+
 
 def snp_based_typing():
     pass
 
+
 def report_generation():
     pass
 
-def multiqc():
+
+def multiqc(out_dir, run_id):
     """
     Create MultiQC report, pulling in outputs from other tools
     """
     print("--------------------------\nMULTIQC\n--------------------------")
-    print("Creating MultiQC report for the analysis")
-    subprocess.run("module load apps/singularity; singularity exec apps/multiqc.sif python -m multiqc output --outdir output/multiqc", shell=True)
+    if os.path.exists(out_dir + "/multiqc/" + run_id):
+        print("MultiQC report already generated for " + run_id)
+    else:
+        print("Creating MultiQC report for the analysis")
+        subprocess.run(
+            "module load apps/singularity; singularity exec apps/multiqc.sif python -m multiqc " + out_dir + " --outdir " +
+            out_dir + "/multiqc/" + run_id, shell=True)
     print("--------------------------")
+
 
 def main():
     # Install containers
@@ -155,19 +193,23 @@ def main():
     # Load singularity
     for run in os.listdir("data/run_folders"):
         if os.path.isdir("data/run_folders/" + run):
-            for summary_file in glob.glob("data/run_folders/" + run + "/final_summary_*.txt"):
+            data_dir = "data/run_folders/" + run
+            for summary_file in glob.glob(data_dir + "/final_summary_*.txt"):
                 run_id = get_identifier(file=summary_file, string="sample_id=").rstrip("\n")
+                out_dir = "output/" + run
                 # create output directory per run, and subdirectories for outputs from each tool
                 create_directory(parent_directory="output", directory=run_id)
-                sub_directories = ["fastqc", "pycoqc", "human_read_removal"]
+                sub_directories = ["fastqc", "pycoqc", "human_read_removal", "de_novo_assembly"]
                 for i in sub_directories:
-                    create_directory(parent_directory="output/" + run_id, directory=i)
-                # Conduct fastQC analysis
-                fastqc(run_folder=run, run_id=run_id)
-                # Conduct pycoQC analysis
-                pycoqc(run_folder=run)
+                    create_directory(parent_directory=out_dir, directory=i)
+                # Conduct QC analysis
+                fastqc(data_dir=data_dir, out_dir=out_dir, run_id=run_id)
+                pycoqc(out_dir=out_dir, data_dir=data_dir)
+                # Conduct alignment and assembly
                 reference_genome = "data/human_genome/ncbi/GCF_000001405.39_GRCh38.p13_genomic.fna"
-                human_read_removal(ref=reference_genome, run_folder=run, run_id=run_id)
+                human_read_removal(data_dir=data_dir, out_dir=out_dir, run_id=run_id, ref=reference_genome)
+                de_novo_assembly(data_dir=data_dir, out_dir=out_dir, run_id=run_id)
+                multiqc(out_dir=out_dir, run_id=run_id)
 
 
 if __name__ == '__main__':
