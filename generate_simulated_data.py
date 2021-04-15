78f939a2858fd6bdd3d6c4700ee20a6deb895dc0
Rachel <rachel.g.duffin@gmail.com>
2021-04-15 17:57:48 +0100

    Create draft script to generate simulated data

---
+++ generate_simulated_data.py
@@ -0,0 +1,13 @@
+import install_containers
+app_dictionary = {
+    "nanosim": "quay.io/biocontainers/nanosim@sha256:d99389f4fafd8a36547cf5c2a6996a97d929482090682b1a4d070c28069d199b",
+    "nanosim_h":
+        "quay.io/biocontainers/nanosim-h@sha256:76e3d6ab85a917623886d04b49504f1c0865dcfb6fa27cf9d8bd1a7145a26150"
+}
+
+def main():
+    for key in app_dictionary:
+        install_containers.install_tools(key, app_dictionary[key])
+
+if __name__ == '__main__':
+    main()
