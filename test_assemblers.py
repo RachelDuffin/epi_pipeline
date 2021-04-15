df57310e1c084cc7b41879bbe5bf6bf31d1e0b4f
Rachel <rachel.g.duffin@gmail.com>
2021-04-15 17:54:16 +0100

    Create draft scripts for testing tools

---
+++ test_assemblers.py
@@ -0,0 +1,111 @@
+import install_containers
+import subprocess
+
+app_dictionary = {
+    "flye": "quay.io/biocontainers/flye@sha256:f895c72298ea3ae568c265cfb575fefeca768c42870cfea0ef3a4cfc35704086",
+    "canu": "quay.io/biocontainers/canu@sha256:b48b52afc355477015ef60bebded3b4ab3d3099bbf5698879de8eb600c9ff1a4",
+    "raven":
+        "quay.io/biocontainers/raven-assembler@sha256:3bc4cc61483cc48243f6b416eaae41f24eb95f75b7a2770f8062c75b5ac53da3",
+    "minimap2":
+        "quay.io/biocontainers/minimap2@sha256:7f95eecc8eeee8ef8ae7e24d1d1a49ee056fb21d72aea4e2def97484f8a206c5",
+    "racon": "quay.io/biocontainers/racon@sha256:16b6aeb33ee2ac726d313a9de3788adff305e24c07bfeee9f1800c060627b414",
+    "medaka": "quay.io/biocontainers/medaka@sha256:6aa52d718af0f48cf6630e88b22cd7187770bbf028ef89bc54ec7fad2ff7a35f"
+}
+
+# Script to compare performance, scalability and accuracy between assembly tools
+
+# PERFORMANCE AND SCALABILITY:
+# Comparison of performance and memory usage between tools (single thread), using /usr/bin/time -v. Compare by:
+# - Wall clock time - elapsed time from start to end of program
+# - CPU time - Time the CPU spends in user mode (running program's code) and kernel mode (executing system calls)
+# - Peak memory usage - max memory used by program during lifetime
+# - Parallel speedup - ratio of time to run program with one thread to time to run with N threads
+# This is run multiple times changing the number of threads and observing corresponding changes in metrics
+# (all tools - flye, canu, raven - have multithreading support). The linux box has 20 cores.
+
+# ACCURACY:
+# Comparing the assembly to the reference genome, using the MUMner package. Compare by:
+# - No. bases in assembly (should be length of ref. genome)
+# - No. contigs (should be 1)
+# - Average identity - % similarity between assembly and reference genome (should be near to 100%)
+# - Coverage - ratio of no. aligned bases in ref to length of ref (should be near to 100%)
+# - No. mismatches - no. single-base differences between assembly and ref (should be 0)
+# - No. indels - number of insertions and deletions between assembly and ref
+
+
+def flye_assembly(filepath, filename, out_dir, threads):
+    """
+    Run test datasets through flye with differing numbers of threads to assess scalability, performance and accuracy.
+    """
+    # scalability/performance
+
+    print("--------------------------\nDE NOVO ASSEMBLY for {}\n--------------------------".format(filepath))
+
+    # overwrite old file
+    time_file = "test_data/output/assemblers/mock_microbial_community/{}_flye_{}_thread.txt".format(filename, threads)
+    time_output = open(time_file, 'w')
+    time_output.close()
+    with open(time_file, 'a') as filetowrite:
+        stdout, stderr = subprocess.Popen("/usr/bin/time -v sudo docker run --rm -v `pwd`:`pwd` -w `pwd` -i -t {} flye "
+                                          "--nano-raw {} --out-dir {} --meta --threads "
+                                          "{}".format(app_dictionary["flye"], filepath, out_dir, threads),
+                                          shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
+        filetowrite.write(stdout.decode('ascii'))
+    print("--------------------------")
+    # accuracy comparison using MUMmer package
+
+
+def canu_assembly(filepath, filename, out_dir, threads):
+    """
+    Run test datasets through canu with differing numbers of threads to assess scalability, performance and accuracy.
+    """
+    # scalability/performance
+
+    # accuracy comparison using MUMmer package
+
+
+def raven_assembly():
+    """
+    Run test datasets through raven with differing numbers of threads to assess scalability, performance and accuracy.
+    """
+    # scalability/performance
+
+    # accuracy comparison using MUMmer package
+
+
+def main():
+    for key in app_dictionary:
+        install_containers.install_tools(key, app_dictionary[key])
+
+    # Comparison with log and even mock community standards
+    input_filepaths = ["/mnt/flavia/rduffin/outbreak_pipeline/test_data/input/mock_microbial_community/"
+                       "Zymo-GridION-EVEN-BB-SN.fq.gz",
+                       "/mnt/flavia/rduffin/outbreak_pipeline/test_data/input/mock_microbial_community/"
+                       "Zymo-GridION-LOG-BB-SN.fq.gz"]
+    output_directory = "/mnt/flavia/rduffin/outbreak_pipeline/test_data/output/assemblers/mock_microbial_community/"
+    # Conduct flye analyses
+    for fq in input_filepaths:
+        sample_name = str(fq).rsplit("/", 1)[1]
+        flye_directory = output_directory + "flye"
+        for thread in [1, 2, 4, 8]:
+            flye_assembly(filepath=fq, filename=sample_name, out_dir=flye_directory, threads=thread)
+    # Conduct canu analyses
+    for fq in input_filepaths:
+        sample_name = str(fq).rsplit("/", 1)[1]
+        canu_directory = output_directory + "canu"
+        for thread in [1, 2, 4, 8]:
+            canu_assembly(filepath=fq, filename=sample_name, out_dir=canu_directory, threads=thread)
+    # Conduct raven analyses
+    for fq in input_filepaths:
+        sample_name = str(fq).rsplit("/", 1)[1]
+        raven_directory = output_directory + "raven"
+        for thread in [1, 2, 4, 8]:
+            raven_assembly(filepath=fq, filename=sample_name, out_dir=raven_directory, threads=thread)
+
+    # Comparison with simulated monomicrobial reads (Nano-Sim H)
+    # Comparison with simulated metagenomic reads (NanoSim meta)
+    # Comparison with 21 E. faecium isolates (monomicrobial samples)
+
+
+if __name__ == '__main__':
+    main()
