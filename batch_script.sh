5bbd5e9cdcd294278d868a03aae5b61e2715d60f
Rachel Duffin <k1927570@login3.pri.rosalind2.alces.network>
2021-10-08 16:17:31 +0100

    Create batch script for slurm job on HPC

--- batch_script.sh
+++ batch_script.sh
@@ -3,5 +3,8 @@
 #SBATCH --output=slurm_stdout_stderr.txt
 #SBATCH --ntasks=80
 #SBATCH --mem=132000
+#SBATCH --time=168:00:00
+#SBATCH --mail-type=begin        # send email when job begins
+#SBATCH --mail-type=end          # send email when job ends
 
 python3 test_assemblers.py
