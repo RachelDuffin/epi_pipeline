74414742455b5e8c142ef89ae9747576fc92e965
RachelDuffin <rachel.g.duffin@gmail.com>
2021-11-22 16:46:03 +0000

    Update data simulation script

--- generate_simulated_data.py
+++ generate_simulated_data.py
@@ -12,7 +12,7 @@ import get_reference_sequences
 import subprocess
 
 tool_dict = {
-    "nanosim": "quay.io/biocontainers/nanosim@sha256:d99389f4fafd8a36547cf5c2a6996a97d929482090682b1a4d070c28069d199b",
+    "nanosim": "quay.io/biocontainers/nanosim@sha256:b3cdc9ffb111069b787c5b2f744f2db9ffe552acdf10ab34428bd4ffd9f179d0",
     "nanosim-h":
         "quay.io/biocontainers/nanosim-h@sha256:76e3d6ab85a917623886d04b49504f1c0865dcfb6fa27cf9d8bd1a7145a26150"
 }
@@ -29,35 +29,44 @@ median_genome_length_dict = {
     "Enterococcus_faecium": 2.92265
 }
 
-def get_error_profile(out_dir):
+def generate_error_profile(infile, out_dir, refs_dir, base_path):
     """
-    Download the e.coli R9 1D error profile from github NanoSim-H repository, commit position 51fc8cd
+    Generate error profile using test data.
     """
-    # if error profile is not already downloaded, download it
-    if not os.path.isdir("{}ecoli_R9_1D".format(out_dir)):
-        error_command = "git clone https://github.com/karel-brinda/NanoSim-H.git && cd NanoSim-H && " \
-                        "git reset --hard 51fc8cd && cd .. && " \
-                        "cp -r NanoSim-H/nanosimh/profiles/ecoli_R9_1D/ ecoli_R9_1D && rm -r -f NanoSim-H"
-        process = subprocess.Popen(error_command, shell=True, universal_newlines=True, bufsize=1, stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE)
+    print("--------------------------\nGENERATE E.FAECIUM ERROR PROFILE\n--------------------------")
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
 
-        stdout, stderr = process.communicate()
+    # get reference genome file
+    for reference in glob.glob("{}*.fna".format(refs_dir)):
+        if "Enterococcus_faecium" in reference:
+            ref_genome = reference
+
+    os.chdir(out_dir)
+
+    # if error profile is not already generated, generate it
+    if not os.listdir(out_dir):
+        error_command = "module load apps/singularity && singularity exec --bind `pwd`:`pwd` {}/nanosim.sif " \
+                        "read_analysis.py genome -i {} -rg {} -a minimap2 -o " \
+                        "Enterococcus_faecium".format(base_path, infile, ref_genome)
+        print(error_command)
+        run_process(error_command)
+
+     #   error_command = "git clone https://github.com/karel-brinda/NanoSim-H.git && cd NanoSim-H && " \
+      #                  "git reset --hard 51fc8cd && cd .. && " \
+       #                 "cp -r NanoSim-H/nanosimh/profiles/ecoli_R9_1D/ ecoli_R9_1D && rm -r -f NanoSim-H"
+    print("--------------------------")
 
 
-def nanosim_h(refs_dir, reads_per_megabase, median_genome_length_dict, base_path):
+def nanosim(refs_dir, reads_per_megabase, genome_length_dict, base_path, out_dir, error_profile_outdir):
     """
     Generate simulated genomic reads per reference genome.
     """
-    out_dir = "{}/input/simulated_monomicrobial_reads/".format(base_path)
-
     if not os.path.isdir(out_dir):
         os.mkdir(out_dir)
 
     # move to out dir as no out_dir option in command
     os.chdir(out_dir)
-
-    # download error profile from github repo
-    get_error_profile(out_dir)
 
     for reference in glob.glob("{}*.fna".format(refs_dir)):
         print("--------------------------\nGENERATE SIMULATED GENOMIC READS {}\n"
@@ -66,7 +75,7 @@ def nanosim_h(refs_dir, reads_per_megabase, median_genome_length_dict, base_path
         for microbe in median_genome_length_dict:
             if microbe in reference:
                 # calculate number of reads for the genome (rounded to nearest integer as required for nanosim-h)
-                read_count = round(median_genome_length_dict[microbe] * reads_per_megabase)
+                read_count = round(genome_length_dict[microbe] * reads_per_megabase)
                 # circular option used as all inputs are circular genomes
 
                 #command = "module load apps/singularity && singularity exec --bind `pwd`:`pwd` " \
@@ -74,17 +83,24 @@ def nanosim_h(refs_dir, reads_per_megabase, median_genome_length_dict, base_path
                 #          "--min-len 65".format(base_path, reference, microbe, read_count)
 
                 command = "module load apps/singularity && singularity exec --bind `pwd`:`pwd` " \
-                          "{}/nanosim.sif simulator.py genome -rg {} -c ecoli_R9_2D -n {} -max 53793 " \
-                          "-min 65 -b guppy -dna_type circular -o {} --fastq".format(base_path, reference,
-                                                                                     read_count, microbe)
-
-                process = subprocess.Popen(command, shell=True, universal_newlines=True, bufsize=1,
-                                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-                stdout, stderr = process.communicate()
+                          "{}/nanosim.sif simulator.py genome -rg {} -c {} -n {} -max 53793 " \
+                          "-min 65 -b guppy -dna_type circular -o {} -t 16 --fastq".format(base_path, reference,
+                                                                                           error_profile_outdir,
+                                                                                           read_count, microbe)
+                print(command)
+                run_process(command)
 
     os.chdir(base_path)
     print("--------------------------")
+
+def run_process(command):
+    """
+    Runs command as subprocess
+    """
+    process = subprocess.Popen(command, shell=True, universal_newlines=True, bufsize=1,
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    stdout, stderr = process.communicate()
 
 def nanosim_metagenome(refs_dir):
     """
@@ -94,27 +110,36 @@ def nanosim_metagenome(refs_dir):
               "simulated_metagenomic_reads"
     os.chdir(out_dir)
     command = "sudo docker run --rm -v `pwd`:`pwd` -w `pwd` -i -t {} nanosim simulator.py metagenome -gl {} -n {} " \
-                  "-max {} -min {} -b guppy".format(tool_dict[nanosim], genome_list, read_number, max_len, min_len)
+                  "-max 53793 -min 65 -med 1503 -b guppy".format(tool_dict[nanosim], genome_list, read_number)
     # create genome list file, use the calculated max/min read lengths from my other script here?? although were they
     # calculated from enterococcus?
-    subprocess.run(command, shell=True)
+    run_process(commmand)
     os.chdir(cwd)
-
 
 def main():
 
     base_path = os.getcwd()
     # reads per megabase calculated using the average read count from test data, and the median read length for
-    # e.faecium from refseq (9/10/2021)
+    # e.faecium from refseq (9/10/2021). 223239/2.92265 = 76382 reads per megabase
     reads_per_megabase = 76382
     for tool in tool_dict:
         test_assemblers.singularity_pull(tool, tool_dict[tool])
 
     # Download reference sequences
     microbial_refs_dir = "{}/input/reference_sequences/".format(base_path)
-    get_reference_sequences.download_sequences(get_reference_sequences.refseq_dict, microbial_refs_dir)
+    input_dir = "/users/k1927570/outbreak_pipeline/msc_project/input/"
+    out_dir = "{}/input/simulated_monomicrobial_reads/".format(base_path)
+    error_profile_outdir = "{}enterococcus_error_profile".format(out_dir)
+    e_faecium_dir = "{}enterococcus_faecium/enterococcus/ef1_bc_75/".format(input_dir)
 
-    nanosim_h(microbial_refs_dir, reads_per_megabase, median_genome_length_dict, base_path)
+    error_profile_input = "/users/k1927570/outbreak_pipeline/msc_project/input/enterococcus_faecium/enterococcus/" \
+                          "ef1_bc_75/210612_EF_R1_barcode07.fq"
+
+    get_reference_sequences.download_sequences(get_reference_sequences.refseq_dict, microbial_refs_dir, base_path)
+
+    generate_error_profile(error_profile_input, error_profile_outdir, microbial_refs_dir, base_path)
+
+    nanosim(microbial_refs_dir, reads_per_megabase, median_genome_length_dict, base_path, out_dir, error_profile_outdir)
     #nanosim_metagenome(refs_dir = microbial_refs_dir)
 
 if __name__ == '__main__':
